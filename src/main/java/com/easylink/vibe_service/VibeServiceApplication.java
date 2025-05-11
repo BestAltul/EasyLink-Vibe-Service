f9a4ee087bda3ba8e03abc45ea085ee2798abeee
Andrey <andrew.saltul@gmail.com>
2025-05-11 09:03:55 -0400

    Initial commit

---
+++ src/main/java/com/easylink/vibe_service/VibeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.easylink.vibe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easylink/vibe_service/VibeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easylink.vibe_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
